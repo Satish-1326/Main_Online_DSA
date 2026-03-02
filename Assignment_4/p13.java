6c06e625c23ba3637c7871801b83e4e8132cbc7f
Satish <dadassatish6@gmail.com>
2026-03-02 21:27:47 +0530

    new

---
+++ Assignment_4/p13.java
@@ -0,0 +1,34 @@
+class Demo{
+	public static void main(String[]args){
+	
+	int n = 5 ;
+
+	for(int i = 1 ; i <= n ; i++){
+		for(int j = 1 ; j <= n ; j++){
+			if((i+j) >= 6){
+				System.out.print("*"+" ");
+				}
+			else{
+				System.out.print("  ");
+				}
+			}
+		System.out.println();
+		}
+	}
+}
+
+/*
+
+OUTPUT :
+
+        *
+      * *
+    * * *
+  * * * *
+* * * * *
+
+TIME COMLEXITY : O(n^2)
+SPACE COMPLEXITY : O(1)
+
+
+*/
