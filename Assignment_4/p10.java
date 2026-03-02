82e1b9c166c8494a63322644a5f9a4a8a0f3070f
Satish <dadassatish6@gmail.com>
2026-03-02 20:16:39 +0530

    new

---
+++ Assignment_4/p10.java
@@ -0,0 +1,28 @@
+class Demo{
+	public static void main(String[]args){
+
+	int n = 5 ;
+
+		for(int i = 1 ; i<=n ; i++){
+			for(int j = 1 ; j <= i ; j++){
+				System.out.print("*"+" ");
+			}
+		System.out.println();
+		}
+	}
+}
+
+/*
+
+OUTPUT :
+
+*
+* *
+* * *
+* * * *
+* * * * *
+
+TIME COMPLEXITY : O(n^2)
+SPACE COMPLEXITY : O(1)
+
+*/
---
+++ Assignment_4/p9.java
@@ -0,0 +1,28 @@
+class Demo{
+	public static void main(String[]args){
+
+	int n = 3 ;
+	int num = 1 ;
+
+		for(int i = 1 ; i <= n ; i++){
+			for(int j = 1 ; j <= n ; j++){
+				System.out.print(num+" ");
+				num++;
+			}
+		System.out.println();
+		}
+	}
+}
+
+/*
+
+OUTPUT :
+
+1 2 3
+4 5 6
+7 8 9
+
+TIME COMPLEXITY : O(n^2)
+SPACE COMPLEXITY : O(1)
+
+*/
