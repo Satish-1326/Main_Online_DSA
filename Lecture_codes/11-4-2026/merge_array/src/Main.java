97c909f94a52dc82a4fb2962d52ff48710f80e6f
Satish <dadassatish6@gmail.com>
2026-04-12 13:12:27 +0530

    quick sort

--- Lecture_codes/11-4-2026/merge_array/src/Main.java
+++ Lecture_codes/11-4-2026/merge_array/src/Main.java
@@ -35,3 +35,58 @@ class Demo{
         }
     }
 }
+
+class Sorting{
+    public static void main(String[] args){
+        int [] arr = {18,8,7,21,9,38,29,12,3,2,1};
+        int low = 0 ;
+        int high = arr.length-1;
+
+        f(arr , low , high);
+
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void f(int [] arr , int low , int high){
+        if(low>= high){
+            return;
+        }
+        int mid = (low+high) / 2;
+
+        f(arr,low,mid);
+        f(arr,mid+1 , high);
+        merge(arr,low,mid,high);
+    }
+    public static void merge(int [] arr , int low , int mid , int high){
+        int [] merged = new int[high-low+1];
+        int blue = low;
+        int green = mid+1;
+        int red = 0;
+
+        while (blue <= mid && green <= high){
+            if(arr[blue] <= arr[green]){
+                merged[red] = arr[blue];
+                red++;
+                blue++;
+            }else {
+                merged[red] = arr[green];
+                red++;
+                green++;
+            }
+        }
+        while (blue <= mid){
+            merged[red] = arr[blue];
+            red++;
+            blue++;
+        }
+        while (green <= high){
+            merged[red] = arr[green];
+            red++;
+            green++;
+        }
+        for (int i = 0; i < merged.length; i++) {
+            arr[low+i] = merged[i];
+        }
+    }
+}
