34575ab0995236972efbe7e2349f4271d64271f0
Satish <dadassatish6@gmail.com>
2026-04-27 21:02:44 +0530

    new

--- Leetcode/Leetcode_169_Majority_Element.java
+++ Leetcode/Leetcode_169_Majority_Element.java
@@ -26,3 +26,62 @@ OUTPUT :
 Element who has most frequency : 3
 
  */
+
+class Leetcode_169_Majority_Element_using_freq{
+    public static void main(String[] args){
+        int [] arr = {2,2,1,1,1,2,2};
+
+        int ans = 0;
+        int result = 0;
+
+        int max = 0 ;
+        for(int i = 0 ; i < arr.length ; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        int [] freq = new int[max+1];
+
+        for (int i = 0; i < arr.length; i++) {
+            freq[arr[i]]++;
+        }
+
+        for (int i = 0; i < freq.length; i++) {
+            if(freq[i] > ans){
+                ans = freq[i];
+                result = i;
+            }
+        }
+        System.out.println(result);
+    }
+}
+
+
+class Leetcode_169_Majority_Element_using_for_loop{
+    public static void main(String[] args) {
+        int[] arr = {2, 2, 1, 1, 1, 2, 2};
+
+        int result = 0;
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int count = 1;
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[i] == arr[j]){
+                    count++;
+                }
+            }
+            if(count > ans){
+                ans = count;
+                result = arr[i];
+            }
+        }
+        System.out.println("Maximum freq elemenet in the array: "+result);
+    }
+}
+/*
+
+OUTPUT :
+
+Maximum freq elemenet in the array: 2
+
+ */
