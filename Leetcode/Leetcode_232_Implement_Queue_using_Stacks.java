a455be60c40a7fc04fd1af4aba3cf63ac689c5b0
Satish <dadassatish6@gmail.com>
2026-08-17 12:39:51 +0530

    new

---
+++ Leetcode/Leetcode_232_Implement_Queue_using_Stacks.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+
+public class Leetcode_232_Implement_Queue_using_Stacks {
+
+}
+class MyQueue{
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+
+    public MyQueue(){
+
+    }
+
+    public void push(int x){
+        s1.push(x);
+    }
+
+    public int pop(){
+        while (!s1.isEmpty()){
+            s2.push(s1.pop());
+        }
+        int val = s2.pop();
+        while (!s2.isEmpty()){
+            s1.push(s2.pop());
+        }
+        return val;
+    }
+
+    public int peek() {
+        while (!s1.isEmpty()){
+            s2.push(s1.pop());
+        }
+        int val = s2.peek();
+        while (!s2.isEmpty()){
+            s1.push(s1.pop());
+        }
+        return val;
+    }
+    
+    public boolean empty(){
+        return s1.isEmpty();
+    }
+}
