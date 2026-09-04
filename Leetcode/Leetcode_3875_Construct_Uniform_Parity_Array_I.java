afe3a069d20a0a5c40625a8ca9601d9c65195dc1
Satish <dadassatish6@gmail.com>
2026-09-04 13:36:31 +0530

    new

---
+++ Leetcode/Leetcode_3875_Construct_Uniform_Parity_Array_I.java
@@ -0,0 +1,10 @@
+public class Leetcode_3875_Construct_Uniform_Parity_Array_I {
+    public static void main(String[] args){
+        int [] nums1 = {2,3};
+
+        System.out.println(uniformArray(nums1));
+    }
+    public static boolean uniformArray(int [] nums1){
+        return true;
+    }
+}
