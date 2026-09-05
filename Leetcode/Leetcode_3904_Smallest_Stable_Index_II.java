be481df4404b6d97391138e06f4d30c7e3240ba0
Satish <dadassatish6@gmail.com>
2026-09-05 08:37:18 +0530

    new

---
+++ Leetcode/Leetcode_3904_Smallest_Stable_Index_II.java
@@ -0,0 +1,28 @@
+public class Leetcode_3904_Smallest_Stable_Index_II {
+    public static void main(String[] args){
+        int [] nums = {5,0,1,4};
+        System.out.println(firstStableIndex(nums,3));
+    }
+    public static int firstStableIndex(int [] nums, int k){
+        int n = nums.length;
+
+        int [] suffixMin = new int[n];
+
+        suffixMin[n-1] = nums[n-1];
+
+        for (int i = n - 2; i >= 0 ; i--) {
+            suffixMin[i] = Math.min(nums[i] , suffixMin[i+1]);
+        }
+
+        int prefixMax = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            prefixMax = Math.max(prefixMax , nums[i]);
+
+            int score = prefixMax - suffixMin[i];
+            if(score <= k){
+                return k;
+            }
+        }
+        return -1;
+    }
+}
